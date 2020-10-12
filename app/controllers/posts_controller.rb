8856f28b10b9c83fad8dfa763a8ddddb7b552700
Masanori Beppu <yaaman01@mbp-mb.local>
2020-10-12 17:47:04 +0900

    postsコントローラーとビューファイルを作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updata
+  end
+
+  def destroy
+  end
+end
